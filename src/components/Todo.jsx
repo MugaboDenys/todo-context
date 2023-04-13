3d0e4f4ed5b0a4c4f27c550626d4b513d4537329
MugaboDenys <mugabodan2020@gmail.com>
2023-04-13 15:56:26 +0200

    added edit

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,27 +4,35 @@ import {IoCheckmarkSharp} from "react-icons/io5"
 import { TodoContext } from "../context/TodoContext";
 
 const Todo = ({children, id, complete}) => {
-    const {toggleCompleted, handleDelete} = useContext(TodoContext);
-    const [edit, setEdit] = useState(false)
+    const {toggleCompleted, handleDelete, todo, setTodo} = useContext(TodoContext);
+    const [edit, setEdit] = useState(false);
+    const [newInput, setNewInput] = useState("")
 
     const handleEdit = () =>{
         setEdit(prevEd=>!prevEd)
+        setNewInput(children)
     }
 
-    const handleUpdate =() =>{
-        console.log(edit)
+    const handleEnter = (event) => {
+        if(event.key === "Enter"){
+            handleUpdate(id)
+        }
+      }
+
+    const handleUpdate =(id) =>{
+        setTodo(prevTodo=> prevTodo.map(obj=> obj.id === id ? {...obj, inputV: newInput} : obj))
         handleEdit()
     }
 
     return ( 
         <div className="flex justify-between group w-[32rem] py-2 pl-2 mt-5 border-b-2">
             <div className="flex gap-x-2">
-                <input type="checkbox" id={id} onChange={()=>{toggleCompleted(id)}} checked={complete} />
-                <label htmlFor={id} className={`${complete && "line-through"}`}>{children}</label>
+                <input type={edit ? "text" : "checkbox"} id={id} onKeyDown={handleEnter} onChange={edit ? (event)=>{setNewInput(event.target.value)} :()=>{toggleCompleted(id)}} value={edit && newInput} checked={complete} className={`${edit && "w-[25rem] outline-none"}`}/>
+                <label htmlFor={id} className={`${complete && "line-through"}`}>{!edit && children}</label>
             </div>
             <div className="flex gap-3">
                 <MdModeEditOutline className={`text-green-600 cursor-pointer ${edit ? "hidden":"block"} `} onClick={handleEdit} />
-                <IoCheckmarkSharp className={`text-green-600 cursor-pointer ${edit ? "block":"hidden"} `} onClick={handleUpdate} />
+                <IoCheckmarkSharp className={`text-green-600 cursor-pointer ${edit ? "block":"hidden"} `} onClick={()=>{handleUpdate(id)}} />
                 <MdDelete className="text-red-600 cursor-pointer " onClick={()=>{handleDelete(id)}}  />
             </div>
         </div>
