2fea0fffd9c4f10f69d6903e56f24719c56992df
MugaboDenys <mugabodan2020@gmail.com>
2023-04-17 16:43:13 +0200

    made requested changes

--- src/context/TodoContext.jsx
+++ src/context/TodoContext.jsx
@@ -3,36 +3,35 @@ import { createContext, useState } from "react";
 
 export const TodoContext = createContext({});
 
-const TodoProvider=({children}) =>{
-    const [todo, setTodo] = useState([])
+const TodoProvider = ({ children }) => {
+  const [todo, setTodo] = useState([]);
+
+  const handleDelete = (id) => setTodo((prevTodo) => prevTodo.filter((obj) => obj.id !== id));
+  
+
+  const toggleCompleted = (id) => {
+    setTodo((prevTodo) => {
+      return prevTodo.map((obj) => {
+        if (obj.id === id) {
+          return {
+            ...obj,
+            complete: !obj.complete,
+          };
+        }
+        return obj;
+      });
+    });
+  };
+
+  const addTodo = (inputV) => setTodo (prevTodo=>[{ id: nanoid(), inputV, complete: false }, ...prevTodo])
     
-      const handleDelete =(id) =>{
-        setTodo(prevTodo => prevTodo.filter((obj)=> obj.id !== id))
-      }
+  return (
+    <TodoContext.Provider
+      value={{ todo, setTodo, addTodo, toggleCompleted, handleDelete }}
+    >
+      {children}
+    </TodoContext.Provider>
+  );
+};
 
-      const toggleCompleted = (id) => {
-        setTodo((prevTodo) => {
-          return prevTodo.map((obj) => {
-            if (obj.id === id) {
-              return {
-                ...obj,
-                complete: !obj.complete,
-              };
-            }
-            return obj;
-          });
-        });
-      };
-
-      const addTodo = (inputV) => {
-        
-      setTodo (prevTodo=>[{ id: nanoid(), inputV, complete: false }, ...prevTodo])
-      };
-    return(
-        <TodoContext.Provider value={{todo, setTodo, addTodo, toggleCompleted, handleDelete}}>
-            {children}
-        </TodoContext.Provider>
-    )
-} 
-
-export default TodoProvider
+export default TodoProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import TodoProvider from "./context/TodoContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <StrictMode>
     <TodoProvider>
       <App />
     </TodoProvider>
-  </React.StrictMode>
+  </StrictMode>
 );
